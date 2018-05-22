16212ceee60168980bb4990349f120a628c0e0ed
Justin Lawrence <justin.lawrence102@gmail.com>
2018-05-22 19:49:23 -0400

    Working on ride suggestion

--- rideTrack/SuggestRideViewController.swift
+++ rideTrack/SuggestRideViewController.swift
@@ -127,16 +127,7 @@ class SuggestRideViewController: UIViewController, DataModelProtocol, UITextFiel
         // Dispose of any resources that can be recreated.
     }
     
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
+
     
 }
 
