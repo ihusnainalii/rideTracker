262d37cf70aabdb68011a8fff160b7eb1d47135f
Mark Lawrence <mark488@ufl.edu>
2019-01-14 12:16:19 -0500

    Started to calculate stats (In the settings, I've added a temporary new view called report card. This is just there for testing purposes) Day in parks stats never delete, each day gets saved forever in firebase (day-in-park/userID/date/today-stats and today-attractions)

---
+++ rideTrack/Data Models/ReportCardData.swift
@@ -0,0 +1,52 @@
+//
+//  ReportCardData.swift
+//  rideTrack
+//
+//  Created by Mark Lawrence on 1/12/19.
+//  Copyright © 2019 Justin Lawrence. All rights reserved.
+//
+
+import UIKit
+
+class ReportCardData: NSObject {
+    
+    var maxHeight: Double!
+    var totalTrackLength: Double!
+    var maxSpeed: Double!
+    var numberOfAttractionsExperienced: Int!
+    var uniqueAttractionCount: Int!
+    var attractionExperiencedMost: String!
+    var manufactureExperiencedMost: String!
+    var scoreCardNumber: Int!
+    var numberOfVisitsToThePark: Int!
+    
+    
+    var numberOfRollerCoasters: Int!
+    var numberOfWaterRides: Int!
+    var numberOfChildrensRides: Int!
+    var numberOfFlatRides: Int!
+    var numberOfTransportRides: Int!
+    var numberOfDarkRides: Int!
+    var numberOfExploreRides: Int!
+    var numberOfSpectaculars: Int!
+    var numberOfShows: Int!
+    var numberOfFilms: Int!
+    var numberOfParades: Int!
+    var numberOfPlayAreas: Int!
+    var numberOfUpcharges: Int!
+    
+    override init() {
+    }
+    
+//    init(score: Int, date: Date, rideID: Int){
+//        self.date = date
+//        self.score = score
+//        self.rideID = rideID
+//    }
+}
+
+
+
+
+    
+
---
+++ rideTrack/Report Card/Report Card Logic.swift
@@ -0,0 +1,9 @@
+//
+//  Report Card Logic.swift
+//  rideTrack
+//
+//  Created by Mark Lawrence on 1/12/19.
+//  Copyright © 2019 Justin Lawrence. All rights reserved.
+//
+
+import Foundation
